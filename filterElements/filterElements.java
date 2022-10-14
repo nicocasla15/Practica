df0e0a5bf6129209216ec12c92075dfe2568877e
nicolas <nicolas1504bon@gmail.com>
2022-10-14 12:40:30 -0300

    practica

--- filterElements/filterElements.java
+++ filterElements/filterElements.java
@@ -8,14 +8,16 @@ public class filterElements {
     public static List<Integer> filterEle(int n, int k, int[] a) {  
         List<Integer> newA = new ArrayList<Integer>();
         
-        for(int i = 0; i < n-1; i++){
-            for(int j = 1; j < n-2; j++){
-                if(a[i] == a[j]){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-i; j++){
+                if(a[i] == a[j+1+i]){
                     if(newA.contains(a[i])){
-                        i += 1;
+                        j += 0;
+                        break;
                     }
                     else{
-                        newA.add(a[i]); 
+                        newA.add(a[i]);
+                        break;
                     }
                 }
             }
@@ -26,7 +28,7 @@ public class filterElements {
     public static void main(String[] args) {
         int n = 9;
         int k = 2;
-        int[] a = {4,5,2,5,4,3,1,3,4};
+        int[] a = {4,5,2,5,4,3,4,1,3,4};
         System.out.println(filterEle(n,k,a));
     }
 }
---
+++ jumpingClouds/jumpingClouds.java
@@ -0,0 +1,33 @@
+package jumpingClouds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class jumpingClouds {
+    
+
+    public static int jumpingInClouds(List<Integer> c,int n){
+        int count = 0;
+        int i = 0;
+        while(i < c.size() - 1) {
+            if(c.get(i + 2) == 1){
+                count++;
+                i++;
+            } else {
+                i += 2;
+                count++;
+            }
+        }
+        return count;    
+    
+    }
+
+    public static void main(String[] args){
+        List<Integer> c = new ArrayList<Integer>();
+        c.add(0);
+        c.add(1);
+        c.add(0);
+        int n = 3;
+        System.out.println(jumpingInClouds(c, n));
+    }
+}
