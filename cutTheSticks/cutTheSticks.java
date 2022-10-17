9795b4fe77b22658343e94bdcd0e027cef3e0b70
nicolas <nicolas1504bon@gmail.com>
2022-10-17 11:19:05 -0300

    practica

--- cutTheSticks/cutTheSticks.java
+++ cutTheSticks/cutTheSticks.java
@@ -1,32 +1,28 @@
 package cutTheSticks;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class cutTheSticks {
 
-    public static List<Integer> cutSticks(List<Integer> arr, int n){
-        Collections.sort(arr);
+    public static List<Integer> cutSticks(int[] arr){
+        Arrays.sort(arr);
+
         List<Integer> arr1 = new ArrayList<Integer>();
+        arr1.add(arr.length);
         
-
-
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] != arr[i-1]){
+                arr1.add(arr.length - i);
+            }
+        }
         return arr1;
-
-
     }
 
     public static void main(String[] args){
-        int n = 6;
-        List<Integer> arr = new ArrayList<Integer>(n);
-        arr.add(5);
-        arr.add(4);
-        arr.add(4);
-        arr.add(2);
-        arr.add(2);
-        arr.add(8);
-        System.out.println(cutSticks(arr,n));
+        int[] arr = new int[]{1,2,3,4,3,3,2,1};
+        System.out.println(cutSticks(arr));
     }
 
 }
