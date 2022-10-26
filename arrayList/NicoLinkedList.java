f37afdfaa59d1848f96a2e43240c75847e120e8b
nicolas <nicolas1504bon@gmail.com>
2022-10-26 12:55:59 -0300

    practica

--- arrayList/NicoLinkedList.java
+++ arrayList/NicoLinkedList.java
@@ -1,27 +1,47 @@
 package arrayList;
 
-import java.util.LinkedList;
+import java.util.Arrays;
 
 public class NicoLinkedList {
-    
-    private static int defaultCapacity = 10;
-    private static int[] array = new int[defaultCapacity];
-    private static int[] newArray;
+
+    private static int[] array;
     int size = 0;
     int cont = 0;
 
     public NicoLinkedList(){
         super();
-        newArray = new int[size];
+        array = new int[3];
     }
 
-    public void add(int element) {
+    public void add(int nod2) {
+        array[cont] = nod2;
+        cont += 1;
+    }
 
+    public String toString() {
+        return ""+Arrays.toString(array);
     }
 
     public static void main(String[] args){
         NicoLinkedList arr = new NicoLinkedList();
-        arr.add(7);
-        arr.add(6);
+        int num1 = 1;
+        int num2 = 3;
+        int num3 = 7;
+
+        Nodo n1 = new Nodo(num1);
+        Nodo n2 = new Nodo(num2);
+        Nodo n3 = new Nodo(num3);
+
+        n1.setPuntero(n2);
+        n2.setPuntero(n3);
+
+        Nodo nod = n1;
+        for(int i = 0; i<3;i++){
+            int nod2 = nod.getElemento();
+            arr.add(nod2);
+            System.out.println(nod2);
+            nod = nod.getPuntero();
+        }
+        System.out.println(arr.toString());
     }
 }
---
+++ arrayList/Nodo.java
@@ -0,0 +1,34 @@
+package arrayList;
+
+public class Nodo {
+    private Nodo puntero;
+    private int elemento;
+
+    public Nodo(int n) {
+        setPuntero(null);
+        setElemento(n);
+    }
+
+    public Nodo(int n, Nodo c) {
+        setPuntero(c);
+        setElemento(n);
+    }
+
+    public void setPuntero(Nodo puntero) {
+        this.puntero = puntero;
+    }
+
+    public Nodo getPuntero() {
+        return puntero;
+    }
+
+    public void setElemento(int elemento) {
+        this.elemento = elemento;
+    }
+
+    public int getElemento() {
+        return elemento;
+    }
+
+    
+}
